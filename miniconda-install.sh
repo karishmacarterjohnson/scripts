5dedaa4a6bef8ef8550d8a262a638c57d4c09bd8
karishmacjohnson <karishmacjohnson@gmail.com>
2020-08-07 18:44:40 -0700

    add existing file

---
+++ miniconda-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# python3 miniconda
+
+cd ~
+
+sudo bash Miniconda3-latest-Linux-x86_64.sh
+# -b to skip the enter?
+# -y to skip the yes?
+# /opt/miniconda3 
+
+# sudo chown -R (user) /opt/miniconda3
+
+# source /opt/miniconda3/bin/activate
